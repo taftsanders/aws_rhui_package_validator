f42d2ea579b5b829e7c16a2e7f4bf12b64eebb61
Taft Sanders <tasander@pos.zircon.local>
2020-03-22 15:34:04 -0400

    made plan for project

--- extract_certs.py
+++ extract_certs.py
@@ -2,12 +2,13 @@ import os
 import rpm
 import wget
 import get_links as gl
+PARENT='/tmp/rhui-client-rpms'
 RHEL6_DIR='/tmp/rhui-client-rpms/rhel6/'
 RHEL7_DIR='/tmp/rhui-client-rpms/rhel7/'
 RHEL8_DIR='/tmp/rhui-client-rpms/rhel8/'
 
 try:
-    if os.mkdir('/tmp/rhui-client-rpms'):
+    if os.mkdir(PARENT):
         os.mkdir(RHEL6_DIR)
         os.mkdir(RHEL7_DIR)
         os.mkdir(RHEL8_DIR)
@@ -17,18 +18,31 @@ except OSError:
 
 def download_rhel6_rpms():
     os.chdir(RHEL6_DIR)
-    gl.get_latest_version()
+    #gl.get_latest_version()
     for rpm in gl.get_rhel6_rpms():
+        os.mkdir(rpm)
+        os.chdir(PARENT + rpms)
         wget.download(rpm)
+        extract(rpm)
 
 def download_rhel7_rpms():
     os.chdir(RHEL7_DIR)
-    gl.get_latest_version()
+    #gl.get_latest_version()
     for rpm in gl.get_rhel7_rpms():
+        os.mkdir(rpm)
+        os.chdir(PARENT + rpm)
         wget.download(rpm)
+        extract(rpm)
 
 def download_rhel8_rpms():
     os.chdir(RHEL8_DIR)
-    gl.get_latest_version()
+    #gl.get_latest_version()
     for rpm in gl.get_rhel8_rpms():
+        os.mkdir(rpm)
+        os.chdir(PARENT + rpm)
         wget.download(rpm)
+        extract(rpm)
+
+def extract():
+    os.system('rpm2cpio ' + rpm + ' | cpio -id')
+    
