f46122e3f130f8d77b842877ef337e1dc4219854
Taft Sanders <tasander@xbox.usersys.redhat.com>
2020-04-19 17:44:28 -0400

    Clean up debug comments added output file for get_repos.get_rhel6_repos()

--- get_repos.py
+++ get_repos.py
@@ -12,20 +12,6 @@ RHEL7_ARCH = ['x86_64']
 RHEL8_RELEASES = ['8']
 RHEL8_ARCH = ['aarch64', 'ppc64le', 's390x', 'x86_64']
 
-def call_repo_url(repo_name, mirror_url):
-    req = urllib3.PoolManager(
-        cert_reqs = 'CERT_REQUIRED',
-        ca_certs='/tmp/rhui-client-rpms/rhui-ca.crt',
-#        )
-        cert_file = '/tmp/rhui-client-rpms/master-content-cert.crt',
-        key_file = '/tmp/rhui-client-rpms/master-content-key.key',
-        )
-    repo = {}
-    baserepo = req.request('GET', mirror_url).data.decode('utf-8')
-    repo[repo_name] = baserepo.split('\n')
-    print(repo)
-    return repo
-
 '''
 step 1: region
 step 2: repo
@@ -39,22 +25,11 @@ def get_rhel6_repos():
         req = urllib3.PoolManager(
             cert_reqs = 'CERT_REQUIRED',
             ca_certs='/tmp/rhui-client-rpms/rhui-ca.crt',
-#           )
             cert_file = '/tmp/rhui-client-rpms/master-content-cert.crt',
             key_file = '/tmp/rhui-client-rpms/master-content-key.key',
             )
         for repo_mirror in gm.rhel6_filtered_repo_mirror:
-            name, url = list(repo_mirror.items())[0]
-#        regions = gl.get_regions()
-#        for region in regions[:len(regions)-2]:
-#            req = urllib3.PoolManager(
-#                cert_reqs = 'CERT_REQUIRED',
-#                ca_certs='/tmp/rhui-client-rpms/rhui-ca.crt',
-#                )
-#                cert_file = '/tmp/rhui-client-rpms/master-content-cert.crt',
-#                key_file = '/tmp/rhui-client-rpms/master-content-key.key',
-#                )
-#Create urllib3.req session HERE            
+            name, url = list(repo_mirror.items())[0]           
             name1, url1 = gm.replace_region(name, url, region)
             for arch in RHEL6_ARCH:
                 name2, url2 = gm.replace_basearch(name1, url1, arch)
