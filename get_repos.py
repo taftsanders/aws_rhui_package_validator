6b555b64caf7b271196eebfa72ea6a3f4eeef67b
Taft Sanders <tasander@xbox.usersys.redhat.com>
2020-04-19 17:28:12 -0400

    Changed poolmanager location to create a poolmanager per region; swapped region and repo in the mirrorlist getter

--- get_repos.py
+++ get_repos.py
@@ -28,19 +28,41 @@ def call_repo_url(repo_name, mirror_url):
 
 '''
 step 1: region
-step 2: arch
-step 3: release version
+step 2: repo
+step 3: arch
+step 4: release version
 '''
 def get_rhel6_repos():
     rhel6_mirrors = []
-    for repo_mirror in gm.rhel6_filtered_repo_mirror:
-        name, url = list(repo_mirror.items())[0]
-        regions = gl.get_regions()
-        for region in regions[:len(regions)-2]:
+    regions = gl.get_regions()
+    for region in regions[:len(regions)-2]:
+        req = urllib3.PoolManager(
+            cert_reqs = 'CERT_REQUIRED',
+            ca_certs='/tmp/rhui-client-rpms/rhui-ca.crt',
+#           )
+            cert_file = '/tmp/rhui-client-rpms/master-content-cert.crt',
+            key_file = '/tmp/rhui-client-rpms/master-content-key.key',
+            )
+        for repo_mirror in gm.rhel6_filtered_repo_mirror:
+            name, url = list(repo_mirror.items())[0]
+#        regions = gl.get_regions()
+#        for region in regions[:len(regions)-2]:
+#            req = urllib3.PoolManager(
+#                cert_reqs = 'CERT_REQUIRED',
+#                ca_certs='/tmp/rhui-client-rpms/rhui-ca.crt',
+#                )
+#                cert_file = '/tmp/rhui-client-rpms/master-content-cert.crt',
+#                key_file = '/tmp/rhui-client-rpms/master-content-key.key',
+#                )
+#Create urllib3.req session HERE            
             name1, url1 = gm.replace_region(name, url, region)
             for arch in RHEL6_ARCH:
                 name2, url2 = gm.replace_basearch(name1, url1, arch)
                 for release in RHEL6_RELEASES:
                     name3, url3 = gm.replace_releasever(name2, url2, release)
-                    rhel6_mirrors.append(call_repo_url(name3, url3))
+                    repo = {}
+                    baserepo = req.request('GET', url3).data.decode('utf-8')
+                    repo[name3] = baserepo.split('\n')
+                    print(repo)
+                    rhel6_mirrors.append(repo)
     return rhel6_mirrors
